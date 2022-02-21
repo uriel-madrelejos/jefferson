229eee9724934e1f75cda75766068e7be5b6f6fe
urieljefferson <urieljefferson@gmail.com>
2022-02-21 04:24:47 -0800

    Add simple three.js scene and torus geometry

--- jefferson/main.js
+++ jefferson/main.js
@@ -1,5 +1,34 @@
 import './style.css'
 
-import * as THREE from 'https://cdn.skypack.dev/three@<version>';
+import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'), 
+    alpha: true
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30); 
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true} );
+
+const torus = new THREE.Mesh ( geometry, material );
+
+scene.add(torus);
+const color = new THREE.Color(1, 0, 0, 0.1)
+function animate(){
+    requestAnimationFrame( animate );
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+    renderer.render( scene, camera );
+}
+
+animate()
